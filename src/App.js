ad2b8fb0f3e5681ac8532431ad369c7e3805dad3
Andre Duvoisin <anduv13@gmail.com>
2019-06-27 00:46:19 -0500

    Add typescript and a sample tsx component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Three from "./Three";
 
 function App() {
   return (
     <div className="App">
+      <Three />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
