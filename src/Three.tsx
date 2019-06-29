3355b91d62fea390b51aa79bfbf0b90864cddf8b
Andre Duvoisin <anduv13@gmail.com>
2019-06-29 12:30:46 -0500

    remove more old and unused code

--- src/Three.tsx
+++ src/Three.tsx
@@ -6,7 +6,6 @@ class Three extends React.Component {
   renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer();
 
   camera: THREE.Camera = new THREE.Camera();
-  // cube: THREE.Mesh;
   geometry: THREE.BufferGeometry = new THREE.BufferGeometry();
   mesh: THREE.SkinnedMesh = new THREE.SkinnedMesh();
   material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial();
@@ -16,14 +15,6 @@ class Three extends React.Component {
   clock: THREE.Clock = new THREE.Clock();
 
   mount: any; // TODO: What is this? How do you strongly type it?
-
-  // constructor(props: any) {
-  //   super(props);
-  //
-  //   // const geometry = new THREE.BoxGeometry(1, 1, 1);
-  //   // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  //   // this.cube = new THREE.Mesh(geometry, material);
-  // }
 
   async componentDidMount() {
     this.onWindowResize();
@@ -452,7 +443,6 @@ class Three extends React.Component {
           // texture.magFilter = THREE.LinearFilter;
           texture.needsUpdate = true;
 
-          // this.material = new THREE.MeshBasicMaterial({ color: 0xff0000 }); // Red material.
           this.material = new THREE.MeshBasicMaterial({
             map: texture,
             skinning: true
