19ff329b80b1edb55ea2011397ce09d6479614c8
Andre Duvoisin <anduv13@gmail.com>
2019-07-01 00:42:04 -0500

    add basic controls. worst state management ever, wow

--- src/ThreeCanvas.tsx
+++ src/ThreeCanvas.tsx
@@ -4,6 +4,7 @@ import CEAssetLoader from "./CEAssetLoader";
 import ThreeAsset from "./ThreeAsset";
 import Stats from "stats.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as dat from "dat.gui";
 
 class Three extends React.Component {
   scene = new THREE.Scene();
@@ -14,7 +15,23 @@ class Three extends React.Component {
   clock = new THREE.Clock();
   controls!: OrbitControls;
 
+  assets: { [name: string]: ThreeAsset } = {};
+
+  skeletonHelper!: THREE.SkeletonHelper;
+
   mixer!: THREE.AnimationMixer;
+
+  // TODO: I'm sure there's a more idiomatic React way to use dat.gui.
+  gui!: dat.GUI;
+
+  modelNames = ["Quarterback Pass", "Thriller Part 2", "Zombie Stand Up"];
+  previousModel = this.modelNames[1];
+  animationIndex = 0;
+  params = {
+    enableMesh: true,
+    enableSkeleton: false,
+    model: this.previousModel
+  };
 
   stats!: Stats;
   statsMount!: HTMLDivElement;
@@ -23,19 +40,22 @@ class Three extends React.Component {
     this.initializeRenderer();
     this.initializeCamera();
     this.initializeOrbitControls();
+    this.initializeGui();
     this.initializeStats();
 
-    const assetLoader = new CEAssetLoader("assets/Quarterback Pass.ceasset");
-    const asset = await assetLoader.load();
-    this.addMeshesToScene(asset.meshes);
-    this.playAnimation(asset, 1);
+    await this.loadAllAssets();
+    this.addMeshesToScene();
+    this.playAnimation(this.animationIndex);
 
-    this.addSkeletonHelperToScene(asset.skeleton);
+    this.addSkeletonHelperToScene();
     this.addGridHelperToScene();
+
+    this.enableMesh(this.params.enableMesh);
+    this.enableSkeleton(this.params.enableSkeleton);
 
     window.addEventListener("resize", this.onWindowResize.bind(this));
 
-    this.animate();
+    await this.animate();
   }
 
   initializeRenderer() {
@@ -55,7 +75,7 @@ class Three extends React.Component {
       0.1,
       10000
     );
-    this.camera.position.set(0, 100, 300);
+    this.camera.position.set(0, 200, 650);
   }
 
   initializeOrbitControls() {
@@ -64,10 +84,71 @@ class Three extends React.Component {
     this.controls.update();
   }
 
+  initializeGui() {
+    this.gui = new dat.GUI();
+
+    this.gui
+      .add(this.params, "enableMesh")
+      .onFinishChange(this.enableMesh.bind(this));
+
+    this.gui
+      .add(this.params, "enableSkeleton")
+      .onFinishChange(this.enableSkeleton.bind(this));
+
+    this.gui
+      .add(this.params, "model", this.modelNames)
+      .onFinishChange(this.changeModel.bind(this));
+  }
+
+  enableMesh(enabled: boolean) {
+    this.assets[this.params.model].meshes.forEach(mesh => {
+      mesh.visible = enabled;
+    });
+  }
+
+  enableSkeleton(enabled: boolean) {
+    this.skeletonHelper.visible = enabled;
+  }
+
+  changeModel(newModelName: string) {
+    this.removeSkeletonHelperFromScene();
+    this.addSkeletonHelperToScene();
+
+    this.stopAnimation(this.animationIndex);
+    this.resetAnimation(this.animationIndex);
+    this.playAnimation(this.animationIndex);
+
+    for (const [name, asset] of Object.entries(this.assets)) {
+      const visible = name === newModelName && this.params.enableMesh;
+      asset.meshes.forEach(mesh => {
+        mesh.visible = visible;
+      });
+    }
+
+    this.previousModel = newModelName;
+  }
+
   initializeStats() {
     this.stats = new Stats();
     this.stats.showPanel(0);
     this.statsMount.appendChild(this.stats.dom);
+  }
+
+  async loadAllAssets() {
+    const promises: Promise<void>[] = [];
+    this.modelNames.forEach(choice => {
+      promises.push(this.loadAsset(choice));
+    });
+    await Promise.all(promises);
+  }
+
+  async loadAsset(name: string) {
+    if (this.assets[name] !== undefined) {
+      return;
+    }
+
+    const assetLoader = new CEAssetLoader(`assets/${name}.ceasset`);
+    this.assets[name] = await assetLoader.load();
   }
 
   onWindowResize() {
@@ -75,23 +156,47 @@ class Three extends React.Component {
     this.camera.aspect = window.innerWidth / window.innerHeight;
   }
 
-  addMeshesToScene(meshes: THREE.SkinnedMesh[]) {
-    meshes.forEach(mesh => {
-      this.scene.add(mesh);
-    });
+  addMeshesToScene() {
+    for (const asset of Object.values(this.assets)) {
+      asset.meshes.forEach(mesh => {
+        mesh.visible = false;
+        this.scene.add(mesh);
+      });
+    }
   }
 
-  playAnimation(asset: ThreeAsset, animationIndex: number) {
-    const rootBone = asset.skeleton.bones[0];
+  playAnimation(animationIndex: number) {
+    const rootBone = this.assets[this.params.model].skeleton.bones[0];
     this.mixer = new THREE.AnimationMixer(rootBone);
-    const action = this.mixer.clipAction(asset.animations[animationIndex]);
+    const action = this.mixer.clipAction(
+      this.assets[this.params.model].animations[animationIndex]
+    );
     action.play();
   }
 
-  addSkeletonHelperToScene(skeleton: THREE.Skeleton) {
-    const rootBone = skeleton.bones[0];
-    const skeletonHelper = new THREE.SkeletonHelper(rootBone);
-    this.scene.add(skeletonHelper);
+  stopAnimation(animationIndex: number) {
+    const action = this.mixer.clipAction(
+      this.assets[this.previousModel].animations[animationIndex]
+    );
+    action.stop();
+  }
+
+  resetAnimation(animationIndex: number) {
+    const action = this.mixer.clipAction(
+      this.assets[this.previousModel].animations[animationIndex]
+    );
+    action.reset();
+  }
+
+  addSkeletonHelperToScene() {
+    const rootBone = this.assets[this.params.model].skeleton.bones[0];
+    this.skeletonHelper = new THREE.SkeletonHelper(rootBone);
+    this.skeletonHelper.visible = this.params.enableSkeleton;
+    this.scene.add(this.skeletonHelper);
+  }
+
+  removeSkeletonHelperFromScene() {
+    this.scene.remove(this.skeletonHelper);
   }
 
   addGridHelperToScene() {
@@ -99,7 +204,7 @@ class Three extends React.Component {
     this.scene.add(gridHelper);
   }
 
-  animate() {
+  async animate() {
     requestAnimationFrame(this.animate.bind(this));
 
     this.stats.begin();
